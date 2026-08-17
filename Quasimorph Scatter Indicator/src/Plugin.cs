146558567ae2855f740258ac6d008824087f26f0
Stunt <01igorok@rambler.ru>
2026-08-17 19:46:35 +0300

    Created project manually and added links to Assembly-CSharp.dll and Unity dlls

---
+++ Quasimorph Scatter Indicator/src/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Quasimorph_Scatter_Indicator
+{
+    public static class Plugin
+    {
+
+        public static ConfigDirectories ConfigDirectories = new ConfigDirectories();
+
+        public static ModConfig Config { get; private set; }
+
+        public static Logger Logger = new Logger();
+
+        [Hook(ModHookType.AfterConfigsLoaded)]
+        public static void AfterConfig(IModContext context)
+        {
+
+            Directory.CreateDirectory(ConfigDirectories.ModPersistenceFolder);
+
+            Config = ModConfig.LoadConfig(ConfigDirectories.ConfigPath);
+
+            new Harmony("Stunt_" + ConfigDirectories.ModAssemblyName).PatchAll();
+        }
+
+    }
+}
